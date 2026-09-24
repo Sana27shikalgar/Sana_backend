64a8927586ca01438d769b03fc8aa936cf1fc8f8
Sana Shikalgar <sanashikalgar@z-aksys.com>
2026-09-24 18:06:47 +0530

    feat: implement server setup and MongoDB connection handling

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import connectDB from "./src/db/index.js";
 import dotenv from "dotenv";
+import app from "./src/app.js";
 
 dotenv.config({
     path: "./.env",
 });
 
-connectDB();
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
+app.on("error", (err) => {
+    console.error("Server error:", err);
+});
+
+connectDB().then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+});
